6bf29ada1a9c29583c213d8a5b29e327eb3feaf2
donatimar <donatimar@gmail.com>
2024-10-15 13:50:12 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,23 @@
+// * TRACCIA
+
+/*
+Scrivi un programma che stampi i numeri da 1 a 100,
+ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz.
+Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
+*/
+
+// * PROCEDURA
+
+// ! RACCOLTA DATI
+
+/* elenco variabili da inserire
+
+/* inserire un numero compreso fra 1 a 100
+
+/* controllo se il numero inserito è valido o meno
+
+// ! ELABORAZIONE DATI
+
+/* inserisco un loop da 1 a 100
+
+/* output finale
