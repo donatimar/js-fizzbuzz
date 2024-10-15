6f87579bcfbfc002e22eee2d2489d6769377b2c2
donatimar <donatimar@gmail.com>
2024-10-15 14:31:24 +0200

    added ciclo for, deleted variables and RACCOLTA DATI beacuse i misread the assignment

--- js/main.js
+++ js/main.js
@@ -8,8 +8,9 @@ Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
 
 // * PROCEDURA
 
-// ! RACCOLTA DATI
+// ! PARTE DI ESERCIZIO COMPLETAMENTE INUTILE CHE HO SCRITTO SBAGLIANDO A LEGGERE LA CONSEGNA, LA LASCIO COMMENTATA AVENDOLA ORMAI GIA' SCRITTA
 
+/*
 // elenco variabili da inserire
 const isValidNumber = (n) => Number.isInteger(n) && n > 0; // variabile per controllare se un numero è valido o meno
 
@@ -27,10 +28,26 @@ if (!isValidNumber(inputNumber)) {
   );
 } else {
   console.log("Valore inserito valido, procedo con l'esercizio");
+*/
 
-  // ! ELABORAZIONE DATI
+// ! ELABORAZIONE DATI + OUTPUT
 
-  // inserisco un loop da 1 a 100
-
-  // output finale
+// inserisco un ciclo da 1 a 100
+for (let i = 1; i <= 100; i++) {
+  // controllo se il numero è divisibile sia per 3 che per 5
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log("FizzBuzz"); // se il numero è divisibile per 3 e per 5
+  }
+  // Controllo se è divisibile solo per 3
+  else if (i % 3 === 0) {
+    console.log("Fizz"); // se il numero è divisibile solamente per 3
+  }
+  // controllo se è divisibile solo per 5
+  else if (i % 5 === 0) {
+    console.log("Buzz"); // se il numero è divisibile solamente per 5
+  }
+  // se non è divisibile né per 3 né per 5, stampo il numero
+  else {
+    console.log(i); // se il numero non è divisibile né per 3 né per 5
+  }
 }
